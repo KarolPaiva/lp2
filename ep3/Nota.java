2722815942e369131c0d49b99d3cfd2c658161f3
KarolPaiva <karollina.paivas@gmail.com>
2017-04-03 10:20:01 -0300

    Notas/Disciplina/Aluno

--- ep3/Nota.java
+++ ep3/Nota.java
@@ -1,5 +1,31 @@
 public class Nota{
-	private double valor;
-	private Aluno alu;
+	private double nota;
+	private Aluno a;
 	private Disciplina d;
 }
+public Nota( double nota, Aluno a, Disciplina d){
+                this.nota = nota;
+                this.d = d;
+		this.a = a;
+        }
+
+        public double  getNota(){
+                return this.nota;
+        }
+        public Disciplina  getDisciplina(){
+                return this.d;
+        }
+        public Aluno  getAluno(){
+                return this.a;
+        }
+	
+	public int compareTo( Nota outra){
+		if( this.nota > outra.getNota())
+			return -1;
+        	else if( this.nota < outra.getNota())
+			return 1;
+		else
+			return 0;
+	}
+}
+
